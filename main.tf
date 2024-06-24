556f600c53c7c8fd64368e743a748498e479f312
polavarapuumesh <polavarapuumesh9@gmail.com>
2024-06-24 22:39:19 +0530

    vpc_ec2

--- main.tf
+++ main.tf
@@ -1,28 +1,40 @@
-provider "aws" {
+provider "aws"{
   region  = "us-east-1"
 }
 
+#vpc
 resource "aws_vpc" "vedantu_network"{
   cidr_block = "10.0.0.0/16"
   instance_tenancy = "default"
+  tags={
+    Name="prod_vpc"
+  }
 }
-# 120 sunet hosts are available
+
+#subnets 
 resource "aws_subnet" "public_subnet_1" {
-  vpc_id     = aws_vpc.vedantu_network.id
-  cidr_block = "10.0.0.0/25"
+  vpc_id = aws_vpc.vedantu_network.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  tags={
+    Name="subnet_1"
+  }
 }
 resource "aws_subnet" "public_subnet_2" {
   vpc_id     = aws_vpc.vedantu_network.id
-  cidr_block = "10.0.0.128/25"
-}
-
-#this is ec2 ami
-resource "aws_instance" "jenkins_server" {
-  ami           = "ami-082283201b8a5468d"
-  instance_type = "t2.micro"
-  key_name="vpc.pem"
-  tags = {
-    Name = "jenkins-server"
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+  tags={
+    Name="subnet_2"
   }
 }
-  
+resource "aws_subnet" "public_subnet_3" {
+  vpc_id     = aws_vpc.vedantu_network.id
+  cidr_block = "10.0.3.0/24"
+  availability_zone = "us-east-1c"
+  tags={
+    Name="subnet_3"
+  }
+}
+
+
